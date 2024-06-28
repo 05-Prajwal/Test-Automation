f35ba349425107cab37bd041fc71f6e5d1f11d4e
05-Prajwal <prajwaldivakar3@gmail.com.com>
2024-06-28 08:34:54 +0530

    Selenium test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebDriver driver = null;
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe");
+        driver=new ChromeDriver(co);
+        driver.get("https://awesomeqa.com/ui/");
     }
 }
